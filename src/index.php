800f5f1359856645c1a911a136cf246ec2277eac
Mitalisri <mitalisrivastava@cedcoss.com>
2022-10-20 12:27:14 +0530

    test

---
+++ src/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $n=1;
+    echo "<table>";
+    for($i=4;$i>=$n;$i--)
+    {
+        echo $i;
+    for($i=4;$i>=$n;$i--)
+    {
+        echo $i;
+    for($i=4;$i>=$n;$i--)
+    {
+        echo $i;
+    for($i=4;$i>=$n;$i--)
+    {
+        echo $i;
+    for($i=4;$i>=$n;$i--)
+    {
+        echo $i; 
+    for($j=$i;$j>=$i;$j--)
+    {
+    
+        echo  $j;
+}
+    }
+}
+}
+}
+    }
+echo "<br>";
+
+for($i=4;$i>$n;$i--)
+{
+    echo $i;
+for($j=$i;$j>=$n;$j--)
+{
+    echo $j;
+}
+}
+echo "<br>";
+for($i=4;$i>$n;$i--)
+{
+    echo "" .$i. "";
+for($j=$i;$j>=$n;$j--)
+{
+    echo "" .$j. "";
+}
+}
+echo "<br>";
+for ($i=4;$i>=$n ;$i--)
+{
+    echo $i;
+for($i=4;$i>$n;$i--)
+{
+    echo "" .$i. "";
+for($j=$i;$j>=$n;$j--)
+{
+    echo "" .$j. "";
+}
+}
+}
+
+
+    
+
+ 
+   
+
+    ?>
+</body>
+</html>
